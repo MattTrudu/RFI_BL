dd290cd6fd2fb8dff2aefa31f1354f410039d7ad
Matt Trudu <trudumatteo@outlook.com>
2023-07-07 10:49:59 +0200

    Updated folder with new changes

--- plot_frb_candidate.py
+++ plot_frb_candidate.py
@@ -291,11 +291,11 @@ def plot_candidate(filename,
     ax0_00.plot(timeseries , color = "darkblue", linewidth = 2)
 
     if (sk_flag is True):
-        ax0_11.plot(offbpass[~badchans], freqs[~badchans], linewidth = 2, color = "darkred", alpha = 0.5)
-        ax0_11.plot(onbpass[~badchans],  freqs[~badchans],linewidth = 2, color = "darkgreen", alpha = 0.9)
+        ax0_11.step(offbpass[~badchans], freqs[~badchans], linewidth = 2, color = "darkred", alpha = 0.5)
+        ax0_11.step(onbpass[~badchans],  freqs[~badchans],linewidth = 2, color = "darkgreen", alpha = 0.9)
     else:
-        ax0_11.plot(offbpass, freqs, linewidth = 2, color = "darkred", alpha = 0.5)
-        ax0_11.plot(onbpass,  freqs,linewidth = 2, color = "darkgreen", alpha = 0.9)
+        ax0_11.step(offbpass, freqs, linewidth = 2, color = "darkred", alpha = 0.5)
+        ax0_11.step(onbpass,  freqs,linewidth = 2, color = "darkgreen", alpha = 0.9)
 
     vmin = np.nanpercentile(dedispdata, 1)
     vmax = np.nanpercentile(dedispdata, 99)
