5445481cb5702f7c6cfe61aa7557ddacf053d053
Matt Trudu <trudumatteo@outlook.com>
2023-07-07 11:46:00 +0200

    Updated folder with new changes

--- plot_frb_candidate.py
+++ plot_frb_candidate.py
@@ -180,6 +180,7 @@ def DMT(dedispdata, freqs, dt, DM = 0, dmsteps = 256, ref_freq = "bottom"):
     dmrange = 0.4 * DM
 
     DMs = np.linspace(-dmrange, dmrange, dmsteps)
+    print(dms)
 
     dmt = np.zeros((dmsteps, dedispdata.shape[1]))
 
