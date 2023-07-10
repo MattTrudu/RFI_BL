ec04c33713c37a7c0ff2a700dec0b6b1872d0741
Matt Trudu <trudumatteo@outlook.com>
2023-07-10 22:08:02 +0200

    Updated folder with new changes

--- clean_h5file.py
+++ clean_h5file.py
@@ -7,6 +7,7 @@ import sys
 if __name__ == "__main__":
     # Read the existing .h5 file using blimpy
     file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
     data = blimpy.Waterfall(file_path)
 
     # Access the data and metadata as needed
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     print(data.shape)
 
     # Create a new .h5 file using h5py
-    output_file_path = 'path/to/your/output_file.h5'
+
     with h5py.File(output_file_path, 'w') as output_file:
         # Create datasets in the new file
         output_file.create_dataset('header', data=header)
