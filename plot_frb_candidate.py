0fc6a7fce7e2a69db13d70ee721337223c8d6b1e
Matt Trudu <trudumatteo@outlook.com>
2023-07-18 14:17:57 +0200

    Changes

--- plot_frb_candidate.py
+++ plot_frb_candidate.py
@@ -484,19 +484,19 @@ def plot_candidate(filename,
     ax0_11.plot(offbpass, freqs, linewidth = 2, color = "darkred", alpha = 0.5)
     ax0_11.plot(onbpass,  freqs,linewidth = 2, color = "darkgreen", alpha = 0.9)
 
-    max_line_width = 40
+    #max_line_width = 40
 
-    wrapped_text = textwrap.fill(f"File directory: {filedir}", width=max_line_width)
+    #wrapped_text = textwrap.fill(f"File directory: {filedir}", width=max_line_width)
     #figure.text(0.650, 0.950, wrapped_text, fontsize=10)
 
 
 
-    figure.text(0.650,0.750, f"File Information" ,fontsize = 10)
+    #figure.text(0.650,0.750, f"File Information" ,fontsize = 10)
 
-    figure.text(0.650,0.700, f"File name: {name}" ,fontsize = 10)
-    figure.text(0.650,0.675, f"File directory: {wrapped_text}" ,fontsize = 310
+    #figure.text(0.650,0.700, f"File name: {name}" ,fontsize = 10)
+    #figure.text(0.650,0.675, f"File directory: {wrapped_text}" ,fontsize = 310
 
-    figure.text(0.650,0.950, f"Candidate Information" ,fontsize = 10)
+    #figure.text(0.650,0.950, f"Candidate Information" ,fontsize = 10)
     figure.text(0.650,0.900, f"Candidate arrival time (s) = {tcand}" ,fontsize = 10)
     figure.text(0.650,0.875, r"Candidate DM (pc$\times$cm$^{-3}$) = " + f"{dmcand}" ,fontsize = 10)
     figure.text(0.650,0.850, f"Candidate peak S/N = {snr:.2f}" ,fontsize = 10)
